7a19e1fd325ac85ea0943f8539e4852f8738ae73
Lê Bảo <lehathaibao@Le-Ha-Thai-Baos-Macbook-Air.local>
2021-06-20 15:38:15 +0700

    added copy button for public key

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import classnames from 'classnames';
 import { get } from 'lodash';
 import { withRouter } from 'next/router';
+import { toast } from 'react-toastify';
+import { FaCopy } from 'react-icons/fa';
 
 import Chain from '../entities/chain';
 import socket from '../utilities/socket';
@@ -34,6 +36,18 @@ const Home = ({ router }) => {
     return <h1>Loading ...</h1>
   }
 
+  const copyPublicKey = () => {
+    const input = document.getElementById('publicKey');
+    input.select();
+    navigator.clipboard.writeText(input.value).then(() => {
+      toast.dark('Public-key copied');
+    })
+      .catch((error) => {
+        toast.error('Can\'t copy, try again later.');
+        console.log({ error });
+      });
+  }
+
   const exit = () => {
     socket.emit(SOCKET_CLIENT_EVENT.USER_DISCONNECTED);
     localStorage.removeItem('credentials');
@@ -45,13 +59,14 @@ const Home = ({ router }) => {
       <h1 className={styles.title}>
         Loyalty Exchange!
       </h1>
-      <p>Welcome{wallet.name && `, ${wallet.name}`}</p>
+      <p>Welcome{wallet.name && `, ${wallet.name}`} <button onClick={copyPublicKey}><FaCopy /><span>(Copy your public-key)</span></button></p>
 
       <h4>Balance: {balance}</h4>
       <button className={classnames(styles.button, styles.buttonDimension)} onClick={() => router.push('/mining')}>Start mining</button>
       <button className={classnames(styles.button, styles.buttonDimension)} onClick={() => router.push('/transaction')}>Make a transaction</button>
       <button className={classnames(styles.button, styles.buttonDimension)} onClick={() => router.push('/transaction-history')}>Transaction history</button>
       <button className={classnames(styles.button, styles.buttonDimension)} onClick={exit}>Exit</button>
+      <input hidden id="publicKey" type="text" value={wallet.publicKey} />
     </div>
   )
 };
